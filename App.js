4419272088bb5c08055181049ffa5a5c028f65e4
Octavio Noval <163550043+OctavioDNoval@users.noreply.github.com>
2025-08-26 02:15:32 -0300

    Add HomeScreen and HeaderComponent components; update App.js to include them

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./src/screens/HomeScreen";
+import HeaderComponent from "./src/components/HeaderComponent";
 
 export default function App() {
 	return (
 		<View style={styles.container}>
+			<HomeScreen />
 			<StatusBar style="auto" />
 		</View>
 	);
